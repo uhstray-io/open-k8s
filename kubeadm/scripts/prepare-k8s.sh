d6bfa98ce30dd641f9eab3fd4dc22f061fdebdaf
Joseph A. Wisneski IV <stray@uhstray.io>
2023-11-28 20:02:42 -0500

    fixed directory targets and updated formatting and syntax for echos

--- kubeadm/scripts/prepare-k8s.sh
+++ kubeadm/scripts/prepare-k8s.sh
@@ -15,16 +15,16 @@ KREL_VERSION="v0.16.2"
 
 # Directories
 CNI_DEST="/opt/cni/bin"
-INSTALL_DIR="/opt/bin"
-KUBECTL_DIR="/opt/bin/kubectl"
+INSTALL_DIR="/usr/local/bin"
+KUBECTL_DIR="/usr/local/bin/kubectl"
 SERVICE_DIR="/etc/systemd/system"
 
-echo "Installing container dependencies...\n"
+echo -e "Installing container dependencies...\n"
 
 # Install CNI Plugins
 sudo mkdir -p "$CNI_DEST"
 curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGINS_VERSION}/cni-plugins-linux-${ARCH}-${CNI_PLUGINS_VERSION}.tgz" | sudo tar -C "$CNI_DEST" -xz
-echo "#########--------CNI Plugins Installed.--------#########"
+echo -e "#########--------CNI Plugins Installed.--------#########\n\n"
 
 # Create the download directory for binaries
 sudo mkdir -p "$INSTALL_DIR"
@@ -48,34 +48,40 @@ sudo systemctl enable --now containerd
 curl -L "https://github.com/opencontainers/runc/releases/download/${RUNC_VERSION}/run.${ARCH}.tar.gz"
 sudo install -m 755 runc.${ARCH} ${INSTALL_DIR}/runc
 
-echo "#########--------Containerd and RUNC Installed.--------#########"
+echo -e "#########--------Containerd and RUNC Installed.--------#########\n\n"
 
 # Download and setup kubeadm and kubelet
-echo "Setting up kubeadm and kubelet...\n"
+echo -e "Setting up kubeadm and kubelet...\n"
 
 cd $INSTALL_DIR
 sudo curl -L --remote-name-all https://dl.k8s.io/release/${K8S_RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet}
 sudo chmod +x {kubeadm,kubelet}
 
 curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${KREL_VERSION}/cmd/krel/templates/latest/kubelet/kubelet.service" | sed "s:/usr/bin:${INSTALL_DIR}:g" | sudo tee ${SERVICE_DIR}/kubelet.service
-sudo mkdir -p /etc/systemd/system/kubelet.service.d
+sudo mkdir -p ${SERVICE_DIR}/kubelet.service.d
 curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${KREL_VERSION}/cmd/krel/templates/latest/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${INSTALL_DIR}:g" | sudo tee ${SERVICE_DIR}/kubelet.service.d/10-kubeadm.conf
 
-echo "#########--------Kubeadm and Kubelet Configured--------#########"
+echo -e "#########--------Kubeadm and Kubelet Configured--------#########\n\n"
 
 # Download, install, and check kubectl
-echo "Installing kubectl...\n"
+echo -e "Installing kubectl...\n"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${ARCH}/kubectl"
+
+echo -e "Validating kubectl binary...\n\n"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/${ARCH}/kubectl.sha256"
+
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
 sudo install -o root -g root -m 0755 kubectl $KUBECTL_DIR
 
-kubectl version --client --output=yaml
+sudo kubectl version --client --output=yaml
 
-echo "###End version output###\n"
-kubectl cluster-info
+echo -e "###End version output###\n\n"
+sudo kubectl cluster-info
 
-echo "###End cluster info###\n"
+echo -e "###End cluster info###\n\n"
 # Enable and start kubelet
-echo "Enabling kubectl with systemctl...\n"
+echo -e "Enabling kubectl with systemctl...\n"
 sudo systemctl enable --now kubelet
 
 echo "#########--------Kubernetes setup complete. Use kubectl to access...--------#########"
