90055e8815c98a31989e35d6d762e55e0945d2d4
Jacob <jacobedwardhaig@gmail.com>
2023-12-19 22:41:39 -0500

    Update echo statements in prepare-k8s.sh script

--- kubeadm/scripts/prepare-k8s.sh
+++ kubeadm/scripts/prepare-k8s.sh
@@ -74,7 +74,7 @@ fi
 
 sudo install -o root -g root -m 0755 kubectl $KUBECTL_DIR
 
-echo -e "##################----------------Kubectl verified and installed----------------##################\n\n"
+echo -e "====================== Kubectl verified and installed ======================\n\n"
 
 
 
@@ -97,7 +97,7 @@ else
 fi
 
 # Check for existing containerd service unit and remove if exists
-echo -e " "Checking for existing containerd service unit...\n"
+echo -e "Checking for existing containerd service unit...\n"
 if [ -f ./containerd.service ]; then 
     echo "Found existing containerd service unit. Removing..."
     rm -f containerd.service
@@ -116,9 +116,9 @@ echo "Reloading systemd daemon and enabling containerd service..."
 sudo systemctl daemon-reload
 sudo systemctl enable --now containerd
 
-echo -e "======================Containerd installed and enabled.======================\n"
+echo -e "====================== Containerd installed and enabled. ======================\n"
 
-echo -e "======================Preparing to install RUNC and CNI Resources....======================\n"
+echo -e "====================== Preparing to install RUNC and CNI Resources.... ======================\n"
 
 
 
@@ -162,7 +162,7 @@ sudo mkdir -p $CNI_DEST
 echo -e "Downloading and installing CNI Resources...\n"
 curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGINS_VERSION}/cni-plugins-linux-${ARCH}-${CNI_PLUGINS_VERSION}.tgz" | sudo tar -C $CNI_DEST -xz
 
-echo -e "##################----------------CNI Plugins and RUNC Installed.----------------##################\n\n"
+echo -e "====================== CNI Plugins and RUNC Installed. ======================\n\n"
 
 
 
@@ -170,42 +170,64 @@ echo -e "##################----------------CNI Plugins and RUNC Installed.------
 # Download and setup kubeadm and kubelet
 echo -e "Setting up kubeadm and kubelet...\n"
 
-# Install crictl for kubeadm and CRI
+# Check if crictl is already downloaded
 if [ -d ${INSTALL_DIR}/crictl-$CRICTL_VERSION-linux-${ARCH} ]; then 
-    echo -e "Proper crictl version already downloaded, skipping...\n" 
+    echo "Proper crictl version already downloaded, skipping..." 
 else 
+    # Remove old crictl checksum file if it exists
     if [ -f ./crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz.sha256 ]; then 
         rm -f crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz.sha256
     fi
 
+    # Download crictl
+    echo -e "Downloading crictl...\n"
+    curl -L "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" 
+    
+    # Download crictl checksum file
+    echo "Downloading crictl checksum..."
     curl -LO "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz.sha256"
 
+    # Check the downloaded crictl file against the checksum
+    echo "Checking crictl checksum..."
     CRICTL_CHECK="$(echo "$(cat crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz.sha256)  crictl" | sha256sum --check)"
-
     echo -e "$CRICTL_CHECK\n"
 
+    # If the checksum is OK, download and extract crictl
     if [ "$CRICTL_CHECK" = "crictl: OK" ]; then 
-        echo -e "SHA256SUM CHECK SUCCESSFUL...\n"
-        wget curl -L "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" | sudo tar -C $INSTALL_DIR -xz
-    else echo -e "SHA256SUM CHECK FAILED... please verify your download URLs for kubectl\n" 
+        echo -e "SHA256SUM CHECK SUCCESSFUL. Downloading crictl...\n"
+        sudo tar Cxzvf $INSTALL_DIR crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz
+    else 
+        echo -e "SHA256SUM CHECK FAILED. Please verify your download URLs for kubectl.\n" 
         exit 1
     fi
 fi
 
+# Change to the install directory
 cd $INSTALL_DIR
+
+# Download kubeadm and kubelet
+echo -e "Downloading kubeadm and kubelet...\n"
 sudo curl -L --remote-name-all https://dl.k8s.io/release/${K8S_RELEASE}/bin/linux/${ARCH}/{kubeadm,kubelet}
 sudo chmod +x {kubeadm,kubelet}
 
+# Download and configure the kubelet service
+echo -e "Configuring kubelet service...\n"
 curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${KREL_VERSION}/cmd/krel/templates/latest/kubelet/kubelet.service" | sed "s:/usr/bin:${INSTALL_DIR}:g" | sudo tee ${SERVICE_DIR}/kubelet.service
 
+# Create the kubelet service directory if it doesn't exist
 if ! [ -f ${SERVICE_DIR}/kubelet.service.d ]; then 
-    echo -e "Creating kubelet.service directory: ${SERVICE_DIR}/kubelet.service.d"
+    echo -e "Creating kubelet.service directory: ${SERVICE_DIR}/kubelet.service.d\n"
     sudo mkdir -p ${SERVICE_DIR}/kubelet.service.d 
 fi
 
+# Download and configure the kubeadm configuration
+echo -e "Configuring kubeadm...\n"
 curl -sSL "https://raw.githubusercontent.com/kubernetes/release/${KREL_VERSION}/cmd/krel/templates/latest/kubeadm/10-kubeadm.conf" | sed "s:/usr/bin:${INSTALL_DIR}:g" | sudo tee ${SERVICE_DIR}/kubelet.service.d/10-kubeadm.conf
 
-echo -e "##################----------------Kubeadm and Kubelet Configured----------------##################\n\n"
+echo -e "Kubeadm and Kubelet configured successfully.\n"
+
+
+
 
 echo -e "Testing cluster resources...\n"
 
@@ -219,4 +241,4 @@ echo -e "###End cluster info###\n\n"
 echo -e "Enabling kubectl with systemctl...\n"
 sudo systemctl enable --now kubelet
 
-echo "##################----------------Kubernetes setup complete. Use kubectl to access...----------------##################"
+echo "====================== Kubernetes setup complete. Use kubectl to access... ======================"
