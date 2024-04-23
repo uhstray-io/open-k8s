332e340cb58a6b85dc2a942bf463640ee64aeb34
Joseph A. Wisneski IV <stray@uhstray.io>
2024-04-23 13:58:47 -0400

    started pseudocode for definemachines.go

---
+++ gopen-k8s/define_machines.go
@@ -0,0 +1,15 @@
+# Choose the start OS image for the machine
+
+# Based on the OS Image select the appropriate user and file directory
+
+# Input user credentials (username, password, and/or ssh-key) for the machine
+
+# Gather the machine's IP address, hostname, and hardware statistics (including CPU, Memory, Disk, etc...)
+
+# Define the control-plane machine's configurations (CPU, Memory, Disk, etc.)
+
+# Define the worker machine's configurations (CPU, Memory, Disk, etc.)
+
+# Use local storage or select a dynamics storage array
+
+# Define the DR machine's configurations (CPU, Memory, Disk, etc.)
